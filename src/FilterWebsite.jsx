cb3c3398473bc45f73591414075a96bf5ed95d46
Ephriam Kassa <ephriam123@gmail.com>
2024-06-28 20:03:29 -0700

    dsadsadsad

--- src/FilterWebsite.jsx
+++ src/FilterWebsite.jsx
@@ -108,11 +108,11 @@ const FilterWebsite = () => {
         <div style={styles.imageContainer}>
           <img src="./chigga.png" alt="Centered Image" style={styles.image} />
           <div style={styles.textOverlay}>
-            <div style={styles.text}>Chigga</div>
+            <div style={styles.text}>Chigger</div>
             <div style={styles.text}>Half N-word</div>
             <div style={styles.text}>Half Asian</div>
           </div>
-          <button onClick={toggleDrawingMode} style={styles.button}>Draw your "chi"</button>
+          <button onClick={toggleDrawingMode} style={styles.button}>Draw your Chigger</button>
         </div>
       )}
       <PaintBrush position={brushPosition} />
